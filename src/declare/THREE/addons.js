926bdd893dac5d6dddf2209854203d449ecaeecc
Zachary Chen <zacchenzy@gmail.com>
2025-09-29 02:17:28 -0400

    delete

--- src/declare/THREE/addons.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addonsBasic = void 0;
-var addonsBasic = /** @class */ (function () {
-    function addonsBasic() {
-    }
-    return addonsBasic;
-}());
-exports.addonsBasic = addonsBasic;
